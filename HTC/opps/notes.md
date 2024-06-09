f9af92d0eb49049f1ab9992002e499b3dccf763d
rajkumar963 <rajkumarsaw597@gmail.com>
2024-06-09 15:18:33 +0530

    code update

---
+++ HTC/opps/notes.md
@@ -0,0 +1,19 @@
+# javascript and classes
+
+# OOP
+# Object
+- collection of property and method
+- toLowerCase
+
+## why use OOps:- object literal(parts of oops)
+
+-constructor function
+-prototype
+-Classes
+-Instance(new,this)
+
+# 4pillors
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
---
+++ HTC/opps/oop.js
@@ -0,0 +1,14 @@
+const user={
+    username:"Prince",
+    loginCount:7,
+    signedIn:true,
+
+    getUserDetails:function(){
+        //console.log("username is name  " + this.username);
+       // console.log(this)
+    }
+   
+}
+//console.log(user.username);
+//console.log(user.getUserDetails());
+
