43f96e8be8f04c3f78dcd56135fbd216f384e9c0
rajkumar963 <rajkumarsaw597@gmail.com>
2024-09-23 20:57:16 +0530

    code update

---
+++ Youtube/ep-42forEach-array-method/script.js
@@ -0,0 +1,10 @@
+const fruits = ['banana', 'apple', 'peach', 'mango', 'grapes']
+
+// fruits.forEach(function(){
+
+//     console.log("hrk")
+// })
+
+fruits.forEach((fruit)=>{ 
+    console.log(fruit)
+})
